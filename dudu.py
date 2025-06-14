3c4f976f974875e2933ff1d44f435e181324ee6e
Vivi <havntme@gmail.com>
2025-06-14 18:39:36 -0400

    added config file for easier adding of servers

--- dudu.py
+++ dudu.py
@@ -3,19 +3,30 @@
 
 import subprocess
 
-#servers
+#username and servers are stored in the "config" file
+#the first line is reserved for username and all subsequent
+#hande server addresses
 
-username = "johndoe"
-servers = ["10.0.0.1", "10.0.0.2", "xxx.xx.xx.x"]
+servers = []
 
-#command arg / command to distribute
+conf = open("config")
+username = conf.readline()
+for x in conf:
 
+    servers.append(x.strip())
+conf.close()
+
+#command arg / command to distribute to servers
+
+#default of "echo", can be overridden at runtime or set here.
 command = "echo"
 
+#executes given command over array of servers
 def run_command(input):
     for x in servers:
         subprocess.run([input, x])
 
+#handles input of command and overriding
 commandOverride = input("input command to override default:\n(leave blank for default)\n")
 if not commandOverride:
     print(f"executing default command: {command}")

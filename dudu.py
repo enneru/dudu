2f2f3fa8a2b6eea25515514c03375feec5793469
Vivi <havntme@gmail.com>
2025-06-14 17:56:32 -0400

    basic python script

---
+++ dudu.py
@@ -0,0 +1,26 @@
+#######DUDU########
+####made by me :3##
+
+import subprocess
+
+#servers
+
+username = "johndoe"
+servers = ["10.0.0.1", "10.0.0.2", "xxx.xx.xx.x"]
+
+#command arg / command to distribute
+
+command = "echo"
+
+def run_command(input):
+    for x in servers:
+        subprocess.run([input, x])
+
+commandOverride = input("input command to override default:\n")
+if not commandOverride:
+    print(f"executing default command: {command}")
+    run_command(command)
+else:
+    print(f"executing overridden command: {commandOverride}")
+    run_command(commandOverride)
+
